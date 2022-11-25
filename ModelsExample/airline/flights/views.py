ebb0e26ce59a1857c895444881edd3cfd5ed24c1
Tarcisio Silva <76399323+Tarcisio2code@users.noreply.github.com>
2022-11-25 12:00:47 +0100

    Add a model to display data for a specific flight

--- ModelsExample/airline/flights/views.py
+++ ModelsExample/airline/flights/views.py
@@ -6,4 +6,9 @@ def index(request):
     return render(request, "flights/index.html", {
         "flights": Flight.objects.all()
     })
-    
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(id=flight_id)
+    return render(request, "flights/flight.html", {
+        "flight": flight
+    })
