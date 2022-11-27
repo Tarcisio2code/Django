a445f25bb54414ec5e11da5eeceb4b019984b28c
Tarcisio Silva <76399323+Tarcisio2code@users.noreply.github.com>
2022-11-27 14:48:06 +0100

    Add passenger info on flight page and flight links

--- ModelsExample/airline/flights/views.py
+++ ModelsExample/airline/flights/views.py
@@ -9,6 +9,8 @@ def index(request):
 
 def flight(request, flight_id):
     flight = Flight.objects.get(id=flight_id)
+    passengers = flight.passengers.all()
     return render(request, "flights/flight.html", {
-        "flight": flight
+        "flight": flight,
+        "Passengers": passengers
     })
