7496c2f59f5f3009020fc9c1eb37e4a6742cd867
Tarcisio Silva <76399323+Tarcisio2code@users.noreply.github.com>
2022-11-27 19:29:40 +0100

    Add Book Flights Form

--- ModelsExample/airline/flights/views.py
+++ ModelsExample/airline/flights/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from .models import Flight
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from .models import Flight, Passenger
 
 # Create your views here.
 def index(request):
@@ -10,7 +13,17 @@ def index(request):
 def flight(request, flight_id):
     flight = Flight.objects.get(id=flight_id)
     passengers = flight.passengers.all()
+    non_passengers = Passenger.objects.exclude(flights=flight).all()
     return render(request, "flights/flight.html", {
         "flight": flight,
-        "Passengers": passengers
+        "Passengers": passengers,
+        "non_passengers": non_passengers
     })
+
+def book(request, flight_id):
+    if request.method == "POST":
+        flight = Flight.objects.get(pk=flight_id)
+        passenger_id = int(request.POST["passenger"])
+        passenger = Passenger.objects.get(pk=passenger_id)
+        passenger.flights.add(flight)
+        return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
